b4c7e097d36a881047caac2ebd428ec4b036e369
humayunalamHO <humayun.alam@digital.homeoffice.gov.uk>
2024-12-24 11:23:58 +0000

    change URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // Set the production url of your site here
 //  url: 'https://github.com',
-  url: 'www.humayundevops.com',
+  url: 'https://www.humayundevops.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
